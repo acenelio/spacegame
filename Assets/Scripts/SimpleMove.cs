cfadf30ea2f2c930190fd73eafbf7b2cf4edfc3b
Sabrina Goncalves <sabrinagpsilva2013@gmail.com>
2019-03-11 08:52:34 -0300

    Primeiro script: Start, Update e posição

---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+    }
+}
