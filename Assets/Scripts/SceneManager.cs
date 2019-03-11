872c8064b5a68ce59e2678ccf3f6bde04b9a4a68
Sabrina Goncalves <sabrinagpsilva2013@gmail.com>
2019-03-11 10:55:13 -0300

    Scrip Scene Manager para mover fita do projeto

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f);
+    [SerializeField]
+    private Transform Tape = null;
+
+ // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tape.position = Tape.position + TapeSpeed * Time.deltaTime;
+    }
+}
