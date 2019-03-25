bcd624e3f66b8988ec8a3b393c607d66b37a6cfd
Sabrina Goncalves <sabrinagpsilva2013@gmail.com>
2019-03-25 09:07:13 -0300

    moedas sumindo

---
+++ Assets/Scripts/SFXManagers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManagers : MonoBehaviour
+{
+    public static SFXManagers instance;
+
+    public GameObject coinParticles;
+
+    void Awake (){
+        if (instance == null){
+            instance = this;
+        }
+    }
+
+    public void ShowCoinParticles(GameObject obj){
+        Instantiate(coinParticles, obj.transform.position, Quaternion.identity);
+    }
+}
