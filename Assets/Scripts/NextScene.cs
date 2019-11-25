c1aeb7b475cf52c2b3b612f08fd9227d4be00a95
NoaAngelik <aninha298.aa@gmail.com>
2019-11-25 10:18:53 -0200

    push por seguranca

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public float waitTime = 3f;
+    public string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Next());
+    }
+
+    IEnumerator Next()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(nextScene);
+    }
+}
